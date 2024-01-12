0e676cdcd092edd2d3540162eb6fdd9f64e2ba36
aasifwali16@gmail.com <aasifwali16@gmail.com>
2024-01-12 23:26:16 +0530

    SetAvatarStarted

---
+++ Public/src/Pages/SetAvatar.jsx
@@ -0,0 +1,14 @@
+import React,{useState, useEffect} from "react";
+import { Link , useNavigate} from "react-router-dom";
+import styled from "styled-components";
+import Logo from "../Assets/Chatappimage.jpg";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from "axios";
+import { registerRoute } from "../Utilities/APIRoutes";
+
+const SetAvatar=()=>{
+return <div>SetAvatar</div>
+
+}
+export default {SetAvatar};
