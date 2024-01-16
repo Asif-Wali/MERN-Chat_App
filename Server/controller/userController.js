7622caf3561fd728cd1424f6d50938ab7c551145
aasifwali16@gmail.com <aasifwali16@gmail.com>
2024-01-17 01:18:50 +0530

    Contact Component Completed

--- Server/controller/userController.js
+++ Server/controller/userController.js
@@ -11,26 +11,33 @@ const bcrypt= require("bcrypt");
         if (emailCheck) return res.json({msg:"Email already used", status:false})
        const HashedPassword=await bcrypt.hash(password, 5);
         const user = await UserModel.create({username, email, password:HashedPassword});
-        delete user.password;
-        return res.json({status: true, user});
+        const returnedUserDetail={
+            username: user.username,
+            email: user.email
+        }
+        return res.json({status: true, returnedUserDetail});
     } catch (error) {
         next(error);
     }
 }
-
-
 module.exports.login=async (req, res, next)=>{
     
     try {
         const {username, password}= req.body;
-        const usernameCheck= await UserModel.findOne({username})
-        if(!usernameCheck) return res.json({msg:"Incorrect Username or Email.", status:false});
-        const PasswordCheck=await bcrypt.compare(password, 5);
+        const user= await UserModel.findOne({username})
+        if(!user) return res.json({msg:"Incorrect Username or Email.", status:false});
+        const PasswordCheck=await bcrypt.compare(password, user.password);
         if (!PasswordCheck){
             return res.json({msg:"Incorrect Password.",status:false})
         }
-        delete user.password;
-        return res.json({status: true, user});
+        const UserPostLogin={
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            avatarImage: user.avatarImage,
+            isAvatarImageSet: user.isAvatarImageSet
+        }
+        return res.json({status: true, UserPostLogin});
     } catch (error) {
         next(error);
     }
@@ -45,6 +52,19 @@ module.exports.setAvatar= async(req, res, next)=>{
             avatarImage
         });
         return res.json({isSet:userData.isAvatarImageSet, image:userData.avatarImage});
+    } catch (error) {
+        next(error);
+    }
+}
+module.exports.getAllUsers= async (req, res, next)=>{
+    try {
+        const users= await UserModel.find({_id:{$ne:req.params.id}}).select([
+            "_id",
+            "username",
+            "email",
+            "avatarImage"
+        ])
+       return res.json(users);
     } catch (error) {
         next(error);
     }
